438a3afe4b2f3baba4f4af9a6963ab0e26d7d3b4
Callum Pick <115214048+cpickcodes@users.noreply.github.com>
2026-03-29 14:39:03 +0100

    Update main.py
    Added chat filter andupdated roles

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-secret_role = "gamer"
+secret_role = ""
+
+filtered_words = ["", ""]
 
 @bot.event
 async def on_ready():
@@ -28,10 +30,15 @@ async def on_member_join(member):
 async def on_message(message):
 	if message.author == bot.user:
 		return
-		
-	if "shit" in message.content.lower():
-		await message.delete()
-		await message.channel.send(f"{message.author.mention} - don't use that word!")
+
+	msg = message.content.lower()
+
+	for word in filtered_words:
+		if word in msg:
+			await message.delete()
+			await message.channel.send(f"{message.author.mention} - don't use that word!")
+		else:
+			return
 		
 	await bot.process_commands(message)
 	
